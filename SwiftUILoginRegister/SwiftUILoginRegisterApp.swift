3808e1927e47172ce7353d4ebe97519bb351880b
dem <>
2022-12-17 17:03:24 +0300

    initial commit

---
+++ SwiftUILoginRegister/SwiftUILoginRegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginRegisterApp.swift
+//  SwiftUILoginRegister
+//
+//  Created by Dilda Ezgi Metincan on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginRegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
